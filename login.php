d45a032183ea4083d766686643617aa01a53bffa
mohammadraza01 <mmraza01@gmail.com>
2024-03-07 14:58:38 +0530

    first email

---
+++ login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Registration and Login</title>
+</head>
+<body>
+
+
+<h2>Login</h2>
+<form action="login.php" method="post">
+  <label for="login_username">Username:</label><br>
+  <input type="text" id="login_username" name="login_username"><br>
+  <label for="login_password">Password:</label><br>
+  <input type="password" id="login_password" name="login_password"><br><br>
+  <input type="submit" value="Login">
+</form>
+</body>
+</html>
